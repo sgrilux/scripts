59d2b6d5495b7c9fd4e3bc124c305073be6f7481
sgrilux <61145297+sgrilux@users.noreply.github.com>
2023-01-24 16:35:14 +0100

    Add generate_s3_test_files.sh

---
+++ aws/generate_s3_test_files.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Generates random files with random data and upload to an S3 Bucket.
+# Used just to test some S3 operations
+
+[[ $# != 1 ]] && echo "Usage: $0 <bucket_name>" && exit 1
+
+N_FILES=10 # Number of files to generate for each folder
+BYTES=200000a # Bytes for each file
+
+S3_DEST_BUCKET=$1
+S3_KEYS="test test1 test2 test3 test4 test5 test6 test7 test8 test9"
+
+# Check first if the bucket exists
+aws s3api head-bucket --bucket ${S3_DEST_BUCKET} 2>/dev/null || echo "Bucket ${S3_DEST_BUCKET} does not exist!" && exit 15
+
+for d in ${S3_KEYS}; do
+    mkdir -p ${d}
+    for i in $(seq 1 ${N_FILES}); do
+        filename=$(openssl rand -hex 12)
+        cat /dev/urandom | head -c 200000 > ${d}/${filename}
+    done
+    aws s3 cp ./test s3://${S3_DEST_BUCKET}/${d} --recursive --quiet
+done
