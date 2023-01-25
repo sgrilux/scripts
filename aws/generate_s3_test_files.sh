b23fbe2b4096406ce91da27659e802bd7cfab304
sgrilux <61145297+sgrilux@users.noreply.github.com>
2023-01-25 11:22:15 +0100

    add function to generate files in parallel

--- aws/generate_s3_test_files.sh
+++ aws/generate_s3_test_files.sh
@@ -5,7 +5,7 @@
 
 [[ $# != 1 ]] && echo "Usage: $0 <bucket_name>" && exit 1
 
-N_FILES=1000
+N_FILES=10000
 BYTES=200000
 
 S3_DEST_BUCKET=$1
@@ -14,13 +14,20 @@ S3_KEYS="test test1 test2 test3 test4 test5 test6 test7 test8 test9"
 # Check first if the bucket exists
 aws s3api head-bucket --bucket ${S3_DEST_BUCKET} || echo "Bucket ${S3_DEST_BUCKET} does not exist!" || exit 15
 
-for d in ${S3_KEYS}; do
-    mkdir -p ${d}
-    for i in $(seq 1 ${N_FILES}); do
-        filename=$(openssl rand -hex 12)
-        cat /dev/urandom | head -c 200000 > ${d}/${filename}
-    done
-    aws s3 cp ./test s3://${S3_DEST_BUCKET}/${d} --recursive --quiet
+generate() {
+    dir=$1
+    n_files=$2
 
-    rm -fr ${d}
+    mkdir -p ${dir}
+    for i in $(seq 1 ${n_files}); do
+        filename=$(openssl rand -hex 12)
+        cat /dev/urandom | head -c 200000 > ${dir}/${filename}
+    done
+    aws s3 cp ./test s3://${S3_DEST_BUCKET}/${dir} --recursive --quiet
+
+    rm -fr ${dir}
+}
+
+for d in ${S3_KEYS}; do
+    generate ${d} ${N_FILES} &
 done
